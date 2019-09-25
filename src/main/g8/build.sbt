8dcaeca7c86a63b84a0e256e14903a65034cf11a
Eric Cartner <eric@westportsolutions.us>
2019-09-25 14:57:38 -0500

    Add logging support

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -9,6 +9,8 @@ lazy val akkaVersion = "$akka_version$"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
   "com.typesafe.akka" %% "akka-testkit" % akkaVersion % Test,
   "org.scalatest" %% "scalatest" % "3.0.8" % Test
 )
--- src/main/g8/src/main/scala/$package$/EchoActor.scala
+++ src/main/g8/src/main/scala/$package$/EchoActor.scala
@@ -1,12 +1,18 @@
 package $package$
 
 import akka.actor.{Actor, Props}
+import akka.event.Logging
 
 object EchoActor {
   def props: Props = Props(new EchoActor)
 }
 
 class EchoActor extends Actor {
+  val log = Logging(context.system, this)
+
+  override def preStart(): Unit = {
+    log.debug("Starting")
+  }
 
   override def receive: Receive = {
     case msg: String => sender() ! msg
